d611ba23b290247ca5583fb22df801faecd8e97e
lhyla <lhylakontakt@gmail.com>
2017-09-17 18:21:38 +0200

    Adding a employe is posible

--- src/com/lhyla/crud/GetDataFromUser.java
+++ src/com/lhyla/crud/GetDataFromUser.java
@@ -1,5 +1,33 @@
 package com.lhyla.crud;
 
+import java.util.Scanner;
+
 public class GetDataFromUser {
 
+	private Scanner scanner;
+
+	public GetDataFromUser(Scanner scanner) {
+		this.scanner = scanner;
+	}
+
+	public String getEmployeFirstName() {
+		System.out.println("First name: ");
+		return getString();
+	}
+
+	public String getEmployeLastName() {
+		System.out.println("Last name: ");
+		return getString();
+	}
+
+	public String getEmployeEmail() {
+		System.out.println("Email: ");
+		return getString();
+	}
+
+	private String getString() {
+		scanner = new Scanner(System.in);
+		String string = scanner.next();
+		return string;
+	}
 }
--- src/com/lhyla/main/Main.java
+++ src/com/lhyla/main/Main.java
@@ -1,15 +1,29 @@
 package com.lhyla.main;
 
+import java.util.Scanner;
+
 import com.lhyla.crud.AddEmploye;
+import com.lhyla.crud.GetDataFromUser;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Main");
-		
+
 		AddEmploye addEmploye = new AddEmploye();
 		
-		addEmploye.addEmploye("Lukasz", "Kowalski", "kowalski@hotmail.com");
+		Scanner scanner = new Scanner(System.in);
+		
+		GetDataFromUser getDataFromUser = new GetDataFromUser(scanner);
+
+		addEmploye.addEmploye(
+				getDataFromUser.getEmployeFirstName(),
+				getDataFromUser.getEmployeLastName(),
+				getDataFromUser.getEmployeEmail());
+		
+		
+
+		scanner.close();
 	}
 
 }
